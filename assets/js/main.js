42653e2f4c662a99075bc2bd2b2be90c9b3a092d
Chetan Chauhan <chauhanchetan82@gmail.com>
2020-04-24 22:48:30 +0530

    Birthday Post Page at /docs/birthday - bdayfoldernamechanged

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,4 +43,21 @@ $(function () {
     	})
     }
   })
+
+  // collapse functionality
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
+
 })
