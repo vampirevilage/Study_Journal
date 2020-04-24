eeb9b9b69290fbd2b4f8d8dc03132f61dfc402f0
Chetan Chauhan <chauhanchetan82@gmail.com>
2020-04-24 23:10:41 +0530

    Birthday Post Page at /docs/birthday - bdayfoldernamechanged

--- assets/js/collapse.js
+++ assets/js/collapse.js
@@ -4,11 +4,11 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+    var content1 = this.nextElementSibling;
+    if (content1.style.display === "block") {
+      content1.style.display = "none";
     } else {
-      content.style.display = "block";
+      content1.style.display = "block";
     }
   });
 }
